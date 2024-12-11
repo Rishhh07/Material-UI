d2afeb495d7efe8f2e0046c3c0c9ba0b86a45e5b
Rishhh07 <rishita0337@gmail.com>
2024-12-11 22:11:40 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+    return (
+        <div>
+            <Button variant="contained">Contained</Button>
+            <Button variant="text">Text</Button>
+            <Button variant="outline">Outline</Button>
+        </div>
+    );
+}
